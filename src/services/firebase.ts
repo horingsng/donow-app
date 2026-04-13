52718854b648268a117d3d1c5b7b8e9bd4890a25
root <root@srv1322890.hstgr.cloud>
2026-04-13 12:59:50 +0000

    更新 Firebase 設定：正確 API Key 同 Storage Bucket

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqS4mYq-zqR9z0qz_9sG9_zqR9z0qz_9s",
+  apiKey: "AIzaSyDdVMNCRzB9cZBHKmeSWvCjlGOsMYAYMrU",
   authDomain: "donow-app-9a2ca.firebaseapp.com",
   projectId: "donow-app-9a2ca",
-  storageBucket: "donow-app-9a2ca-storage",
+  storageBucket: "donow-app-9a2ca.firebasestorage.app",
   messagingSenderId: "1044613297121",
-  appId: "1:1044613297121:web:xxx"
+  appId: "1:1044613297121:ios:039650993e5d2017cfd8a0"
 };
 
 const app = initializeApp(firebaseConfig);
